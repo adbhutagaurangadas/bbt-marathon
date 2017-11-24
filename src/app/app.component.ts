47b4bb655c787783fbd856abcf783ada30bd4b2f
Adbhuta Gauranga das <adbhutagaurangadas.gkg@gmail.com>
2017-11-24 18:29:27 +0300

    security update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '';
+  uid = 2;
+  uname = '';
+  uphone = '';
+  uemail = '';
+  udescr = '';
+  totalBooks = 0;
+  totalPrice = 0;
+  loading = true;
+  state = 0;
+  showInfo = false;
+  db = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('api/db/'+this.uid).subscribe(data => {
+      this.loading = false;
+      this.db = Object.values(data);
+      this.db.forEach(function(v, i, arr) {
+        arr[i].books.forEach(function(v, i, arr) {
+          arr[i].box = '';
+          arr[i].byone = '';
+        });
+      });
+    });
+  }
+  RecountTotal() {
+    var totalBooks = 0;
+    var totalPrice = 0;
+    this.db.forEach(function (item, i, arr) {
+      item.books.forEach(function (item, i, arr) {
+        totalBooks += (item.box * item.pack) + (item.byone * 1);
+        totalPrice += item.price * ((item.box * item.pack) + (item.byone * 1));
+      });
+    });
+    this.totalBooks = totalBooks;
+    this.totalPrice = totalPrice;
+  }
+  Order() {
+    this.state = 2;
+    var order = [];
+    this.db.forEach(function (item, i, arr) {
+      item.books.forEach(function (item, i, arr) {
+        if((item.box * item.pack) + (item.byone * 1) > 0) {
+          order.push(item);
+        }
+      });
+    });
+    var o = {
+      uid: this.uid,
+      name: this.uname,
+      phone: this.uphone,
+      email: this.uemail,
+      descr: this.udescr,
+      order: order
+    };
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json; charset=utf-8');
+    this.http.post('api/order', JSON.stringify(o), {
+      headers: headers
+    }).subscribe(data => { this.state = 3; });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import {
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatCardModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +28,23 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
